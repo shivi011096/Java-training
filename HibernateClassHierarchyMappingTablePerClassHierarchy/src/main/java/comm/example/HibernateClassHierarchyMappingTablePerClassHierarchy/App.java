c0e59bd1c36a8e936a633cef9dd6f8a4940aed44
Alchemy <Alchemy@192.168.1.24>
2020-01-23 17:23:28 +0530

    23/01/2020

---
+++ HibernateClassHierarchyMappingTablePerClassHierarchy/src/main/java/comm/example/HibernateClassHierarchyMappingTablePerClassHierarchy/App.java
@@ -0,0 +1,13 @@
+package comm.example.HibernateClassHierarchyMappingTablePerClassHierarchy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
