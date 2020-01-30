c2c66eefd5d910daf41908e80259611e20109e88
Alchemy <Alchemy@192.168.1.24>
2020-01-30 17:51:29 +0530

    28/01/2020

---
+++ cmr/src/main/java/crm/service/CustomerServiceImpl.java
@@ -0,0 +1,27 @@
+package crm.service;
+
+import java.util.List;
+
+import crm.dao.CustomerDAO;
+import crm.dao.CustomerDAOImpl;
+import crm.model.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+	private CustomerDAO dao;
+	{
+		dao=new CustomerDAOImpl();
+	}
+
+	@Override
+	public Customer createCustomer(Customer customer) {
+		
+		return dao.createCustomer(customer);
+	}
+
+	@Override
+	public List<Customer> getAllCustomer() {
+		// TODO Auto-generated method stub
+		return dao.getAllCustomer();
+	}
+
+}
